abbaa4e4300e2236541edce0c8cca84585a1450b
Monish <monishkamwal06@gmail.com>
2025-09-13 16:29:28 -0230

    Data Merged from all anomaly files

--- manual_code.py
+++ manual_code.py
@@ -4,6 +4,18 @@ from scipy import stats
 from datetime import datetime
 import matplotlib.pyplot as plt 
 
+# Thesis Timeframe
+start_date = pd.to_datetime('2003-01-01')
+end_date = pd.to_datetime('2014-05-31')
+
+# Regime Periods
+# Regime classification
+regime_periods = {
+    'Pre-Crisis': ('2003-01-01', '2007-11-30'),
+    'Crisis': ('2007-12-01', '2009-06-30'),
+    'Post-Crisis': ('2009-07-01', '2014-05-31')
+}
+
 # Files
 files = {
     'Accruals' : 'Accruals.csv',
@@ -25,7 +37,19 @@ for anomaly, file in files.items():
     df = df[['Date', anomaly]]
     df['Date'] = pd.to_datetime(df['Date'], format='mixed')
     df = df.dropna(subset=[anomaly])
-    df.info()
-    df.head()
+    #df.info()
+    #df.head()
     print('\n \n')
-    
+    if data.empty:
+        data = df
+    else:
+        data = pd.merge(data, df, on='Date', how='outer')
+
+    #data.info()
+    #data.head()
+    print('\n \n')
+
+print(data.head())
+print(data.tail())
+data.info()
+
